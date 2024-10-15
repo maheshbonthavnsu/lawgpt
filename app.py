87780f4350e9244e5787513265304ffa46dc6959
maheshbonthavnsu <128718374+maheshbonthavnsu@users.noreply.github.com>
2024-10-15 18:27:15 +0530

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ import time
 st.set_page_config(page_title="LawGPT")
 col1, col2, col3 = st.columns([1,4,1])
 with col2:
-    st.image("https://github.com/maheshbonthavnsu/LawGPT/blob/main/ai.jpg?raw=true")
+    st.image("https://github.com/maheshbonthavnsu/lawgpt/blob/main/images.jpg?raw=true")
 
 st.markdown(
     """
